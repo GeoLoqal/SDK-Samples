a65d2f2aa95d6a5abf26e8dbc9973b17b0218936
Ron@GeoLoqal <support@geoloqal.com>
2012-12-10 19:17:11 -0500

    Delete

--- Android/GeoloqalSdkv2.4/src/com/geoloqal/com/data/ApplicationInfo.java
+++
@@ -1,9 +0,0 @@
-package com.geoloqal.com.data;
-
-import java.util.ArrayList;
-
-public class ApplicationInfo {
-
-	public static ArrayList<GLLocation> locationData= new ArrayList<GLLocation>();
-	public static  String triggerStatus=null;
-}
